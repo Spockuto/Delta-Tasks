ba2e11ccad057703fadc6934fc233c8e6302e4e0
Venkkatesh Sekar <Spockuto@users.noreply.github.com>
2015-07-12 14:57:12 +0530

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -2,12 +2,9 @@
 echo Enter the full path
 read path
 cd $path
-for i in {1..100} ;
-do
-	mkdir folder$i
-        chmod 700 folder$i
-	cd folder$i
-        touch folder$i.txt
-	chmod 700 folder$i.txt
-	cd ..
-done
+sudo mkdir folder{1..100}
+sudo touch folder{1..100}.txt
+ls -1 | grep -v ..txt|xargs -I {} sudo mv {}.txt {}
+cd ..
+sudo chmod -R 700 .
+sudo -k

